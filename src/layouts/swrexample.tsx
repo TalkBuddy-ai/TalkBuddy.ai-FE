9864d4f9ab6e942ecd96fd490df1b70e5009f70f
Lama Ibrahim <librahim@asaltech.com>
2023-04-11 11:36:24 +0200

    remove swrExample file

--- src/layouts/swrexample.tsx
+++
@@ -1,8 +0,0 @@
-import {useFetch} from '@/customHooks/useSwr'
-
-const Example = () => {
-    const { data, error, loading } = useFetch({ url: '/chats', payload: { prompt: 'hello' }})
-    console.log(data)
-}
-
-export default Example
